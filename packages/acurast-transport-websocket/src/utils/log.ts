5b9743b0550b232371fb664268b72df7a2d4d138
Julia Samol <j.samol@papers.ch>
2023-11-02 15:59:11 +0100

    Release/1.0.0

--- packages/acurast-transport-websocket/src/utils/log.ts
+++ packages/acurast-transport-websocket/src/utils/log.ts
@@ -1,5 +1,11 @@
 import { serializable } from './serialization'
 
 export function log(tag: string, ...data: any[]): void {
-    console.log(tag, ...serializable(data))
+    if (data.length > 0) {
+        console.groupCollapsed(tag)
+        console.log(...serializable(data))
+        console.groupEnd()
+    } else {
+        console.log(tag)
+    }
 }
