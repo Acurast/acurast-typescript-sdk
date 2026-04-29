915b0556891b0d8402071f3e67b6c11344422b61
Andreas Gassmann <andreas@andreasgassmann.ch>
2026-04-29 11:05:54 +0200

    feat: independent packages

--- npm-ci-publish.sh
+++ npm-ci-publish.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
+set -e
+
 echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > .npmrc
 
 git update-index --assume-unchanged npm-ci-publish.sh
 git update-index --assume-unchanged npm-ci-publish-beta-only.sh
 
-VERSION=$(node -pe 'JSON.parse(process.argv[1]).version.indexOf("beta")' "$(cat lerna.json)")
+HAS_BETA=$(node -e "
+const fs = require('fs');
+const path = require('path');
+const pkgs = fs.readdirSync('packages').filter(d => fs.existsSync(path.join('packages', d, 'package.json')));
+const beta = pkgs.some(d => {
+  const v = JSON.parse(fs.readFileSync(path.join('packages', d, 'package.json'))).version;
+  return v.includes('beta');
+});
+console.log(beta ? '1' : '0');
+")
 
-if [ "$VERSION" = "-1" ]
+if [ "$HAS_BETA" = "1" ]
 then
-  npx lerna publish from-package --contents ./ --yes
-else
-  echo "version is beta, using --tag next"
+  echo "beta version detected, using --dist-tag next"
   npx lerna publish from-package --contents ./ --dist-tag next --yes
+else
+  npx lerna publish from-package --contents ./ --yes
 fi
 
 rm .npmrc
